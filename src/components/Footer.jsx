a1e788c0fa07f6a744a8d3f57c720b2b6a7b1149
Santhosh -v <161921479+santhoshvncet@users.noreply.github.com>
2025-06-30 22:52:13 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,10 +6,10 @@ const Footer = () => {
       <footer className="mb-0 text-center">
         <div className="d-flex align-items-center justify-content-center pb-5">
           <div className="col-md-6">
-            <p className="mb-3 mb-md-0">Made with ❤️ by {" "}
+            <p className="mb-3 mb-md-0">Made with ❤️ by {"Santhosh v"}
               <a  href="https://sahibsingh.dev" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Sahib Singh</a>
             </p>
-            <a className="text-dark fs-4" href="https://github.com/ssahibsingh" target="_blank" rel="noreferrer">
+            <a className="text-dark fs-4" href="https://github.com/santhoshvncet/" target="_blank" rel="noreferrer">
               <i className="fa fa-github"></i>
             </a>
           </div>
